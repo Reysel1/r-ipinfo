d16ba866c0fba03dfb3192a348504ef350b9d7cd
Reysel <98265287+Reysel1@users.noreply.github.com>
2022-05-30 07:19:14 +0200

    Add files via upload

---
+++ r-ipinfo/static/script.js
@@ -0,0 +1,39 @@
+let ip = document.getElementById("ip")
+let country = document.getElementById("country")
+let capital = document.getElementById("capital")
+let city = document.getElementById("city")
+let timezone = document.getElementById("timezone")
+let region = document.getElementById("region")
+let org = document.getElementById("org")
+
+function search() {
+    let ip = document.getElementById("ip").value;
+
+    fetch(`https://ipapi.co/${ip}/json/`)
+    .then(response => response.json())
+    .then(function(data) {
+        ip.innerHTML = `IP: ${data.ip}`
+        country.innerHTML = `Country: ${data.country_name}`
+        capital.innerHTML = `Capital: ${data.country_capital}`
+        city.innerHTML = `City: ${data.city}`
+        timezone.innerHTML = `Timezone: ${data.timezone}`
+        region.innerHTML = `Region: ${data.region}`
+        org.innerHTML = `ISP: ${data.org}`
+    })
+}
+
+
+$(function(){
+
+    $("#__hidecont").hide();
+
+        $("#__btnshow").click(function(){
+            $(".card").show();
+    
+    })
+})
+
+
+
+
+
